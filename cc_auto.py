ca963091ebc1530232e49318e92771a2b6ab0133
yuriif671 <yuriif767@deltalearns.ca>
2023-12-01 12:54:53 -0800

    Done

--- cc_auto.py
+++ cc_auto.py
@@ -1,20 +1,36 @@
 import pyautogui as py
 import keyboard
+import time
+
+def clickCookie():
+    py.moveTo('assets/bigcookie.png')
+    for i in range(300):
+        py.click()
+
+def purchaseBuildings():
+    n = 0
+    y = 850
+
+    while n < 9:
+        py.moveTo(1250, y)
+        for i in range(7):
+            time.sleep(0.3)
+            py.click()
+
+        y -= 70
+        n += 1
 
 def main():
     py.alert("Click 'Cookie' to activate CoockieClickerAuto", "CookieClickerAuto", "Cookie")
+    
     keyboard.press_and_release('Alt + Tab')
 
     while True:
         try:
-            py.moveTo('assets/bigcookie.png')
-            for i in range(150):
-                py.click()
-            py.moveTo('assets/cursor.png')
-            py.click()
-            print("clicked the cursor")
+            clickCookie()
+            purchaseBuildings()
         except Exception as e:
-            print("error")
+            print(e)
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-import pyautogui as p
+import pyautogui as py
 
+py.alert("Click 'Cookie' to activate CoockieClickerAuto", "CookieClickerAuto", "Cookie")
 
-p.moveTo('assets/test.png')
+n = 0
+y = 850
+while n < 9:
+    py.moveTo(1250, y, 2)
+    y -= 70
+    n += 1
